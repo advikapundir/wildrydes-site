4638343fb0083490a3d103510d5d5cfeeffa62f3
Advika Pundir <advikapundir0316@gmail.com>
2026-06-18 19:52:15 +0530

    test ride info

--- js/ride.js
+++ js/ride.js
@@ -28,35 +28,16 @@ document.getElementById('rideButton').addEventListener('click', async () => {
     const data = await response.json();
     console.log("Ride data:", data);
 document.getElementById('rideInfo').innerHTML = `
-<div class="confirmation-card">
-
-    <div class="confirmation-header">
-        <div class="unicorn-badge">🦄</div>
-        <h3>Unicorn Assigned</h3>
-        <p>Your magical ride is on its way</p>
-    </div>
-
-   <div class="ride-detail">
-    <span class="ride-label">Ride ID</span>
-    <span class="ride-value ride-id">${data.RideId}</span>
-</div>
-
-<div class="ride-detail">
-    <span class="ride-label">Rider</span>
-    <span class="ride-value rider-email">${data.Rider}</span>
-</div>
-
-    <div class="ride-detail">
-        <span class="ride-label">Color</span>
-        <span class="ride-value">${data.Unicorn.Color}</span>
-    </div>
-
-    <div class="ride-detail">
-        <span class="ride-label">ETA</span>
-        <span class="ride-value">${data.Eta}</span>
-    </div>
-
-
+<div style="
+background:red;
+color:white;
+padding:20px;
+border-radius:20px;
+">
+<h2>🦄 Unicorn Assigned</h2>
+<p>Ride ID: ${data.RideId}</p>
+<p>Rider: ${data.Rider}</p>
+<p>ETA: ${data.Eta}</p>
 </div>
 `;
 });
