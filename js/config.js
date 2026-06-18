ae8e53880e7ec8edd2854ca0dd9cc4b307135adb
Advika Pundir <advikapundir0316@gmail.com>
2026-06-18 11:49:58 +0530

    Added API Gateway URL

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-north-1_fir60Lr1z',
+        userPoolClientId: '1ove0kdqp78mlis9h24t0j2lru',
+        region: 'eu-north-1'
+    },
+    api: {
+        invokeUrl: 'https://m2w6uj9oik.execute-api.eu-north-1.amazonaws.com/prod'
+    }
+};
