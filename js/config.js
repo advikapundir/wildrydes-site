4eb2d7d64d8b5f1172136b7cba6ae642262be413
Advika Pundir <advikapundir0316@gmail.com>
2026-06-18 12:16:52 +0530

    Update Cognito public client

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_fir60Lr1z',
-        userPoolClientId: '1ove0kdqp78mlis9h24t0j2lru',
+        userPoolClientId: '1rq7ea4o4pn38rq92sp45h8ok4',
         region: 'eu-north-1'
     },
     api: {
